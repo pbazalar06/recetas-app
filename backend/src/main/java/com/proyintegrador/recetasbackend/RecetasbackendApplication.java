29d251a82758267c0a50b121cfc21a2af52828f6
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2026-05-05 09:46:56 -0500

    Primer commit - estructura inicial del proyecto

---
+++ backend/src/main/java/com/proyintegrador/recetasbackend/RecetasbackendApplication.java
@@ -0,0 +1,13 @@
+package com.proyintegrador.recetasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/proyintegrador/recetasbackend/RecetasbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyintegrador.recetasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
