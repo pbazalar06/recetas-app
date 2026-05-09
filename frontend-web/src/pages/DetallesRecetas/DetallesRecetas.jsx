7410d95414c71727bac30e3a92cdaeb526c68d5a
pbazalar06 <bazalarvps964@gmail.com>
2026-05-09 15:38:16 -0500

    Estructura semi-completa

---
+++ frontend-web/src/pages/DetallesRecetas/DetallesRecetas.jsx
@@ -0,0 +1,9 @@
+function DetallesRecetas() {
+    return (
+        <div>
+            <h1>DetallesRecetas</h1>
+        </div>
+    );
+}
+
+export default DetallesRecetas;
---
+++ frontend-web/src/pages/Recetas/Recetas.jsx
@@ -0,0 +1,9 @@
+function Recetas() {
+    return (
+        <div>
+            <h1>Recetas</h1>
+        </div>
+    );
+}
+
+export default Recetas;
